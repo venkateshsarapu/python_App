d915e5c7395dac42f2fe2d02ef3588727ea0f1d5
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-02-07 04:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello, Harness! Your Flask app is running successfully."
+    return "Hello, Harness!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
